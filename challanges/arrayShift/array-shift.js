6e7909ff084387a62b74bb338b959b80582cc731
asharaloran96 <asharoran96@gmail.com>
2020-01-22 16:59:21 +0200

    do another branch and solve the test on it

---
+++ challanges/arrayShift/array-shift.js
@@ -0,0 +1,22 @@
+'use strict';
+///////// we work as group in this to get the Idea 
+function insertShiftArray(arr, val) {
+    let divideArr = Math.ceil(arr.length / 2);
+//// we need to increase the arr one index to the new value (arr.length + 1)
+let newArr = new Array(arr.length + 1);
+for (let i = 0; i <= divideArr.length; i++) {
+    if (i === divideArr) {
+        newArr[i] = val;
+    } else {
+        newArr[i] = arr[i];
+    }
+}
+////////// then take the divideArr part 2 
+for (let j = divideArr; j <= arr.length - 1; j++) {
+    newArr[j + 1] = arr[j];
+}
+return newArr;
+}
+
+module.exports = insertShiftArray;
+//////// I will do another solution by my self in weekend after I solve the jest installation
