ef5a4bb9fe5307ee20ae1266ca6fc4d72c420bab
asharaloran96 <asharoran96@gmail.com>
2020-02-09 23:53:45 +0200

    do fizzbuzz folder

---
+++ challanges/fizzBuzzTree/fizz-buzz-tree.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line strict
+'use strict';
+
+const tree = require
+
+// If the value is divisible by 3, replace the value with “Fizz”
+// If the value is divisible by 5, replace the value with “Buzz”
+// If the value is divisible by 3 and 5, replace the value with “FizzBuzz”
+// If the value is not divisible by 3 or 5, simply turn the number into a String.
+FizzBuzzTree(tree){
+    
+}
